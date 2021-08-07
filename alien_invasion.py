da7a42a0d333067322915530f983a79541ebf5ae
Mohit Chakraborty <programming.mohit@gmail.com>
2021-08-07 10:48:08 +0530

    Added ship module to game directory; Modified alien_invasion toshow ship on screen

--- alien_invasion.py
+++ alien_invasion.py
@@ -4,6 +4,7 @@ import sys
 import pygame
 
 from settings import Settings
+from ship import Ship
 
 class AlienInvasion:
 	"""Overall class to manage game assets and behaviour"""
@@ -17,6 +18,8 @@ class AlienInvasion:
 		self.screen = pygame.display.set_mode(
 			(self.settings.screen_width,self.settings.screen_height))
 		pygame.display.set_caption("Alien Invasion")
+
+		self.ship = Ship(self)
 		# Set the background colour
 
 	def run_game(self):
@@ -29,6 +32,7 @@ class AlienInvasion:
 
 			#Redraw the screen during each pass through the loop
 			self.screen.fill(self.settings.bg_color)
+			self.ship.blitme()
 
 			# Make the most recently drawn screen visible.
 			pygame.display.flip()
